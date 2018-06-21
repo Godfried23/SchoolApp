5cbae028bbcc949f54538e95fd635138b41fe123
godfried weihs <godfried23@hotmail.com>
2018-06-20 23:29:41 -0600

    Initial commit

---
+++ SchoolLibrary/School.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SchoolLibrary
+{
+    class School
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zip { get; set; }
+        public string PhoneNumber { get; set; }
+
+        private string _twitterAddress; //backing variable
+        public string TwitterAddress
+        {
+            //make sure the twitter address starts with @
+            get { return _twitterAddress; }
+            set
+            {
+                if (value.StartsWith("@"))
+                {
+                    _twitterAddress = value;
+                } else
+                {
+                    throw new Exception("The Twitter address must begin with @");
+                }
+            }
+        }
+
+        public School()
+        {
+            Name = "Untitled School";
+            PhoneNumber = "555-1234";
+        }
+
+        public School(string SchoolName, string SchoolPhoneNumber)
+        {
+            Name = SchoolName;
+            PhoneNumber = SchoolPhoneNumber;
+        }
+
+        //public float AverageThreeScores(float a, float b, float c)
+        //{
+        //    var result = (a + b + c) / 3;
+        //    return result;
+        //}
+
+        //function bodied expression
+        //generally should only be used for very simple logic, like what is being done here
+        public static float AverageThreeScores(float a, float b, float c) => (a + b + c) / 3;
+
+        public static int AverageThreeScores(int a, int b, int c)
+        {
+            var result = (a + b + c) / 3;
+            return result;
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine(Name);
+            sb.AppendLine(Address);
+            sb.AppendLine(City);
+            sb.Append(", ");
+            sb.Append(State);
+            sb.Append(" ");
+            sb.Append(Zip);
+            return sb.ToString();
+        }
+    }
+}
